6742258901a1c61776f6e83dd6c3fce15e81e9a3
Dilks44 <Dilks44@gmail.com>
2016-10-24 12:08:57 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,283 @@
+
+<!DOCTYPE html>
+<html lang="en" class="full">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author: Andrew Dilks" content="">
+
+  <title> Zeta Psi UMD - Phi Epsilon Chapter</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="http://zetapsiumd.com/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <!-- This needs to be Changed!!! Look in images folder -->
+  <link rel="Shortcut Icon" href="images/favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+
+  <!-- Start of the Top NAVBAR -->
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="http://zetapsiumd.com/">Zeta Psi</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="http://zetapsiumd.com/infopage.php">Exec</a></li>
+          <li><a href="http://zetapsiumd.com/infopageTest.php">Test</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+  <!-- End Of NAVBAR -->
+
+
+  <!-- Main page -->
+
+  <!-- Header TKPhi -->
+  <div class="container">
+    <div class="col-md-12 text-center" style="margin-bottom: 50px;">
+      <h1 class="head1">ZETA PSI</h1> <br>
+      <h1 class="head2">Phi Epsilon Chapter - University of Maryland</h1>
+    </div>
+  </div>
+
+
+  <!-- Front Page Content-->
+  <div class="container">
+    
+    <!-- top row 3 -->
+    <div class="row">
+
+      <!-- About US -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/PorchPicSpring15.gif" alt="Should be: PorchPicSpring15.gif">
+          <div class="caption">
+            <h3>About Us</h3>
+            <p>
+              The Zeta Psi Fraternity of North America was founded June 1, 1847
+               as a social college fraternity. The organization now comprises
+                about 50 active chapters encompassing roughly 20,000 brothers.
+                 Instead of establishing itself at simply any campus, Zeta Psi
+                  limits itself to an elite and select few institutions that
+                   will only yield the best and the brightest Brothers.<br><br>
+
+              Zeta Psi was the FIRST INTERCONTINENTAL Fraternity, with our newly chartered chapter at the prestigious Oxford University.<br>
+
+              Zeta Psi was the FIRST and ONLY Fraternity to simultaneously have a chapter at EVERY IVY LEAGUE university.<br>
+
+              Zeta Psi was the FIRST INTERNATIONAL Fraternity, with a chapter established at The University of Toronto in 1879.<br>
+
+              Zeta Psi was the FIRST Fraternity on BOTH COASTS, with a chapter established at The University of California at Berkeley in 1870.<br><br>
+
+              Zeta Psi seeks the right kind of man - smart, determined,
+               honorable gentlemen - to help ensure the legacy of our 166 year
+                old Brotherhood. We boast leaders in every arena of private and
+                 public service, from chief executives to military commanders to
+                  Presidential hopefuls, renowned lawyers to leading scientists
+                   to influential writers. The caliber of Zeta Psi's exclusive
+                    fraternal network speaks for itself. 
+            </p>
+              <!--
+              <p>
+                <a href="http://zetapsiumd.com/currentlydown.php" class="btn btn-primary" role="button">Read More</a>
+              </p>
+              -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Rush -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/rushzeteresize.jpg" alt="Rush Zeta psi">
+          <div class="caption">
+            <h3>Rush</h3>
+            <div class="well well-sm">
+              <p>
+                <strong> Schedule - Fall 2016</strong> <br>
+                <br>
+                <strong> Meet the Greeks </strong> <br>
+                August 30th 4:30-6:30pm <br>
+                @ McKeldin Mall <br>
+                <br>
+                <strong> Chick Fil A Open House </strong> <br>
+                August 31st 4-7pm <br>
+                @ 13 Fraternity row <br>
+                <br>
+                <strong> Brotherhood Dinner (Invite Only) </strong> <br>
+                September 8th 6-8pm <br>
+                @ 13 Fraternity Row <br>
+                <br>
+                Recruitment Chair:<br>
+                <strong>Elijah Biggs </strong> <br>
+                301 - 876 - 3312 <br>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Philanthropy -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/terpthon.jpg" alt="Should be: terpthon.jpg">
+          <div class="caption">
+            <h3>Philanthropy</h3>
+            <p>
+              Zeta Psi members frequently participate in philanthropic
+               endeavours around campus and in the local community.
+                 During the 2013-2014 year, Zeta Psi brothers at the University
+                  of Maryland came together to raise over $30,000 for various
+                   non-profits and performed hundreds of hours of community
+                    service.
+
+              <br>
+              <br>
+
+              Our fall philanthropy consists of a walk to raise money for the
+               National Eating Disorders Association.  This walk has raise
+                nearly $20,000 and hosted close to 500 participants in its two
+                 years thus far.  Our chapter received the Most Outstanding
+                  Philanthropic Programming Award from the Department of
+                   Fraternity and Sorority Life in 2013 for this event.
+
+              <br>
+              <br>
+
+              Our spring semester philanthropic efforts focus around the
+               University of Maryland's dance marathon, Terp Thon, which raises
+                money for Children's National Hospital in Washington DC. 
+                 We host a dunk tank fundraiser each year with many of the
+                  fraternity and sorority presidnets and council executive board
+                   members to help our Terp Thon fundraising.  This past year,
+                    Zeta Psi was the top fundraising fraternity for Terp Thon.
+            </p>
+              <!--
+              <p>
+                <a href="http://zetapsiumd.com/currentlydown.php" class="btn btn-primary" role="button">Read More</a>
+              </p>
+              -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end of top row -->
+
+    <!-- start of bottom row -->
+    <div class="row">
+
+      <!-- Alumni Section-->
+      <div class="col-sm-6 col-md-4 col-md-push-2">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/alumni.jpg" alt="alumni.jpg">
+          <div class="caption">
+            <h3>Alumni</h3>
+            <p>
+              Becoming a brother of Zeta Psi is a lifelong commitment that does
+              not end when a brother graduates college.  We are proud to boast
+              one of the strongest alumni networks in world.  Our chapter 
+              interacts fequently with our chapter alumni as well as the 
+              nearby DC Elders association in both a professional and a social
+              manner.  Our alumni frequently place active brothers with
+              prestigious jobs and internships.  For more information 
+              regarding our alumni programming, please contact our Alpha Sigma 
+              Ryan Mahaffey at
+              <a href="mailto:Ryan.Mahaffey@ZetaPsi.com">Ryan.Mahaffey@ZetaPsi.com</a>.
+            </p>
+              <!--
+              <p>
+                <a href="http://zetapsiumd.com/currentlydown.php" class="btn btn-primary" role="button">Read More</a>
+              </p>
+              -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Chapter House-->
+      <div class="col-sm-6 col-md-4 col-md-push-2">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/bootstrap/css/images/houseSnow.jpg" alt="Should be: Chapterhouse.jpg">
+          <div class="caption">
+            <h3>Chapter House</h3>
+            <p>
+
+              Our chapter house is the hub for all fraternity activities.
+                It features a large basement perfect for chapter meetings
+                 and registered social events, a conference/study room that
+                  provides a great place for brothers to do homework, and a
+                   living room with a pool table, foosball table, and a large
+                    flat screen tv.  Our in-house chef cooks delicious, healthy
+                     meals for all residents.  At full capacity, our chapter
+                      house can hold over 30 brothers and is one of the most
+                       sought after residences in all of College Park.<br><br>
+            </p>
+            <div class="well well-sm">
+              Address: <br>
+              <strong>
+                Zeta Psi Fraternity <br>
+                13 Fraternity Row <br>
+                College Park, MD 20740 <br>
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end of bottom row -->
+
+
+
+  </div> <!-- end of main page container -->
+
+  <div class="container text-center">
+    <div class="whiteround">
+      <!-- Spacer-->
+      <div class="container"> <div class="push50"> </div> </div>
+      <!-- EndSpacer -->
+
+      <p>
+        Questions, Comments, Website Suggestions, or Problems? Contact our site admin 
+         <a href="mailto:Dilks44@gmail.com?Subject=%7CZETEWebsiteContact%7C%20Question%20for%20you" target="_top">Here</a>
+      </p>
+
+      <!-- Spacer-->
+      <div class="container"> <div class="push50"> </div> </div>
+      <!-- EndSpacer -->
+      
+    </div>
+  </div>
+
+
+
+
+  <!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://zetapsiumd.com/bootstrap/js/bootstrap.min.js"></script>
+  <script src="http://zetapsiumd.com/bootstrap/assets/js/docs.min.js"></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="http://zetapsiumd.com/bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
+
+
+
+
+
+  
---
+++ infopage.php
@@ -0,0 +1,310 @@
+
+<!DOCTYPE html>
+<html lang="en" class="full">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author: Andrew Dilks" content="">
+
+  <title>Zeta Psi UMD - More Info</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="http://zetapsiumd.com/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <!-- This needs to be Changed!!! Look in images folder -->
+  <link rel="Shortcut Icon" href="images/favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+
+ <!-- Start of the Top NAVBAR -->
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" 
+        data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="http://zetapsiumd.com/">Zeta Psi</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="http://zetapsiumd.com/infopage.php">Exec</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+  <!-- End Of NAVBAR -->
+
+
+  <!-- Main page -->
+
+  <!-- Header TKPhi -->
+  <div class="container">
+    <div class="col-md-12 text-center" style="margin-bottom: 50px;">
+      <h1 class="head1">ZETA PSI</h1> <br>
+      <h1 class="head2">Phi Epsilon Chapter - University of Maryland</h1>
+    </div>
+  </div>
+
+
+
+  <!-- Executive Section-->
+  <div class="container text-center">
+    <div class="whiteround">
+
+      <!-- Spacer-->
+      <div class="container"> <div class="push50"> </div> </div>
+
+      <h2>
+        Executive Board
+      </h2>
+
+      <!-- Spacer-->
+      <div class="container"> <div class="push50"> </div> </div>
+
+    </div>
+  </div>
+
+  <!-- Spacer-->
+  <div class="container"> <div class="push50"> </div> </div>
+
+
+
+
+  <div class="container">
+    <div class="row">
+
+    <!-- President position -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/DomVP.jpg" alt="Dom VP">
+          <div class="caption">
+            <h2>Chapter President</h2>
+            <h3>Dominic LoPiccolo</h3>
+            <p>
+               Dominic LoPiccolo is a senior Mechanical Engineering major from
+               South Hackensack NJ. He joined Zeta Psi in the Fall of 2014.
+            </p>
+            <!--
+              <a href=""
+                class="btn btn-primary" role="button">LinkedIn
+              </a>
+            -->  
+          </div>
+        </div>
+      </div>
+
+      <!-- Vice President position -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/FahmeedIslam.jpg" alt="VP">
+          <div class="caption">
+            <h2>Vice President</h2>
+            <h3>Fahmeed Islam</h3>
+            <p>
+              Fahmeed Islam is a junior, biochemistry and neurophysiology major.
+              Upon graduation, he aspires to go to medical school with hopes of
+              becoming an orthopedic surgeon. In his free time, he enjoys
+              playing sports, working out, and making music under his
+              alias, DJ Smores.
+            </p>
+            <p>
+              <!--
+              <a href=""
+                class="btn btn-primary" role="button">LinkedIn</a>
+              -->
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Secretary position -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/TommyLipset.jpg" alt="Secretary">
+          <div class="caption">
+            <h2>Secretary</h2>
+            <h3>Tommy Lipset</h3>
+            <p>
+              Tommy Lipsett is a junior Accounting major from Ocean City, NJ.
+               He has been a Zete since Spring of 2015.
+            </p>
+              <a href="https://www.linkedin.com/in/thomas-lipsett-8a8880a6"
+               class="btn btn-primary" role="button">LinkedIn</a>
+          </div>
+        </div>
+      </div>
+
+    </div> <!-- row -->
+
+    <!-- second row -->
+    <div class="row">
+
+      <!-- Corresponding Secretary -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/JakeKincer.jpg"
+               alt="Corresponding Sec">
+          <div class="caption">
+            <h2>Corresponding Secretary</h2>
+            <h3>Jake Kincer</h3>
+            <p>
+              Senior economics major. Is starting graduate school to obtain a
+               public policy degree. Passionate about international affairs
+                and hopes to work in international development.
+            </p>
+            <!--
+            <a href=""class="btn btn-primary" role="button">LinkedIn</a>
+            -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Treasurer -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/RJWalton.jpg" alt="Treasurer">
+          <div class="caption">
+            <h2>Treasurer</h2>
+            <h3>RJ Walton</h3>
+            <p>
+             RJ Walton is a senior Finance and Supply Chain Managament from Colts
+              Neck NJ. He joined Zeta Psi in the Fall of 2013
+            </p>
+            <!--
+              <a href="#" class="btn btn-primary" role="button">LinkedIn</a>
+            -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Historian -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/JohnTurcotte.jpg" alt="Historian">
+          <div class="caption">
+            <h2>Historian</h2>
+            <h3>Jonathan Turcotte</h3>
+            <p>
+              Chill guy. Enjoys hanging out with friends. Likes to have fun.
+            </p>
+            <!--
+              <a href="#" class="btn btn-primary" role="button">LinkedIn</a>
+            -->
+          </div>
+        </div>
+      </div>
+
+    </div> <!-- row 2 -->
+
+    <!-- Third row -->
+    <div class="row">
+
+      <!-- Social Chair -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/GrahamBuck.jpg" alt="Social Chair">
+          <div class="caption">
+            <h2>Social Chair</h2>
+            <h3>Graham Buck</h3>
+            <p>
+              text
+            </p>
+            <!--
+              <a href="#" class="btn btn-primary" role="button">LinkedIn</a>
+            -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Rush Chair -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/ElijahBiggs.jpg" alt="Rush Captain">
+          <div class="caption">
+            <h2>Rush Chair</h2>
+            <h3>Elijah Biggs</h3>
+            <p>
+              Elijah is a senior bioengineering student who joined Zeta Psi in fall 2014. He likes talking to guys.
+            </p>
+            <!--
+              <a href="#" class="btn btn-primary" role="button">LinkedIn</a>
+            -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Sergeant at Arms -->
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="http://zetapsiumd.com/images/FacePhotos/RaySample.JPG" alt="Sergeant at Arms">
+          <div class="caption">
+            <h2>Sergeant at Arms</h2>
+            <h3>Ray Sample</h3>
+            <p>
+             Senior criminal justice major. Upon graduation, I aspire to work
+             for a federal law enforcement agency. In my free time I enjoy
+             playing baseball
+            </p>
+              <a href="https://www.linkedin.com/in/Raekwon-Sample-5a583a128"
+               class="btn btn-primary" role="button">LinkedIn</a>
+          </div>
+        </div>
+      </div>
+
+    </div> <!-- row 3 -->
+
+
+  </div> <!-- End of content container -->
+
+
+
+
+  <div class="container text-center">
+    <div class="whiteround">
+      <!-- Spacer-->
+      <div class="container"> <div class="push50"> </div> </div>
+      <!-- EndSpacer -->
+
+      <p>
+        Questions, Comments, Website Suggestions, or Problems? Contact our site admin 
+         <a href="mailto:Dilks44@gmail.com?Subject=%7CZETEWebsiteContact%7C%20Question%20for%20you" target="_top">Here</a>
+      </p>
+
+      <!-- Spacer-->
+      <div class="container"> <div class="push50"> </div> </div>
+      <!-- EndSpacer -->
+
+    </div>
+  </div>
+
+
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://zetapsiumd.com/bootstrap/js/bootstrap.min.js"></script>
+    <script src="http://zetapsiumd.com/bootstrap/assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://zetapsiumd.com/bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
